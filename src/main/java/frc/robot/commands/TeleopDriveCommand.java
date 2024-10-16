81506ff94ac371cd80d09f1611fc3493a1ca3459
Aceius E. <io@acei.us>
2024-10-15 17:35:44 -0700

    consider the following

--- src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -9,10 +9,12 @@ import frc.robot.subsystems.Drivetrain;
 public class TeleopDriveCommand extends Command {
     Drivetrain drivetrain;
     CommandXboxController controller;
+    Boolean fieldRelative;
 
-    public TeleopDriveCommand(Drivetrain drivetrain, CommandXboxController controller) {
+    public TeleopDriveCommand(Drivetrain drivetrain, CommandXboxController controller, Boolean fieldRelative) {
         this.drivetrain = drivetrain;
         this.controller = controller;
+        this.fieldRelative = fieldRelative;
     }
 
     /**
@@ -25,7 +27,32 @@ public class TeleopDriveCommand extends Command {
 
     @Override
     public void execute() {
-        drivetrain.setSpeeds(new ChassisSpeeds(controller.getLeftX(), controller.getRightX(), controller.getRightX()));
+        if (fieldRelative) {
+
+        } else {
+            drivetrain.setSpeeds(
+                    new ChassisSpeeds(controller.getLeftX(), controller.getRightX(), controller.getRightX()));
+        }
+
+        // Consider the following
+        // double rcw = pJoystick -> GetTwist();
+        // double forwrd = pJoystick -> GetY() * -1; /* Invert stick Y axis */
+        // double strafe = pJoystick -> GetX();
+
+        // float pi = 3.1415926;
+
+        // /* Adjust Joystick X/Y inputs by navX MXP yaw angle */
+
+        // double gyro_degrees = ahrs -> GetYaw();
+        // float gyro_radians = gyro_degrees * pi / 180;
+        // float temp = forwrd * cos(gyro_radians) +
+        // strafe * sin(gyro_radians);
+        // strafe = -forwrd * sin(gyro_radians) +
+        // strafe * cos(gyro_radians);
+        // fwd = temp;
+
+        // /* At this point, Joystick X/Y (strafe/forwrd) vectors have been */
+        /* rotated by the gyro angle, and can be sent to drive system */
     }
 
     @Override
